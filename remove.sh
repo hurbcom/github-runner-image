4e1f00d3b701ea66aa25dcb2fa2f50b6ffa2f35d
Leandro Tux <leandro.silva@hurb.com>
2021-05-07 12:53:41 -0300

    We need only the token on start and on stop. Creating a script to remove the runner

---
+++ remove.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+export TOKEN=$(curl  -X POST 2>/dev/null  -H "Authorization: token ${GITHUB_ACCESS_TOKEN}"  -H "Accept: application/vnd.github.v3+json"  https://api.github.com/orgs/${ORG}/actions/runners/registration-token | jq '.token' | sed s/\"//g)
+/actions-runner/config.sh remove --token ${TOKEN}
--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 TOKEN=$(curl  -X POST 2>/dev/null  -H "Authorization: token ${GITHUB_ACCESS_TOKEN}"  -H "Accept: application/vnd.github.v3+json"  https://api.github.com/orgs/${ORG}/actions/runners/registration-token | jq '.token' | sed s/\"//g)
-export TOKEN
+
 /actions-runner/config.sh --unattended --labels ${LABELS} --name ${HOSTNAME} --url "https://github.com/${ORG}" --token "${TOKEN}"
 
 exec $@
