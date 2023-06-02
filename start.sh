92ff91fcd8907ae6868615b65969c92de60abdca
Leandro Silva <leandro.silva@hurb.com>
2023-06-02 15:09:55 -0300

    Include an "exit 0" to safe exit

--- start.sh
+++ start.sh
@@ -5,3 +5,5 @@ TOKEN=$(curl  -X POST 2>/dev/null  -H "Authorization: token ${GITHUB_ACCESS_TOKE
 /actions-runner/config.sh --unattended --labels ${LABELS} --name ${HOSTNAME} --url "https://github.com/${ORG}" --token "${TOKEN}" --ephemeral --disableupdate --replace
 
 /actions-runner/run.sh
+
+exit 0
