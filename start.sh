bda46a81f15f922ac6d550612746935af5e0f25b
Leandro Silva <leandro.silva@hurb.com>
2023-07-21 12:45:06 -0300

    Since some versions are configured to deprecated early, disabling it to avoid runners hangs

--- start.sh
+++ start.sh
@@ -2,7 +2,7 @@
 
 TOKEN=$(curl  -X POST 2>/dev/null  -H "Authorization: token ${GITHUB_ACCESS_TOKEN}"  -H "Accept: application/vnd.github.v3+json"  https://api.github.com/orgs/${ORG}/actions/runners/registration-token | jq '.token' | sed s/\"//g)
 
-/actions-runner/config.sh --unattended --labels ${LABELS} --name ${HOSTNAME} --url "https://github.com/${ORG}" --token "${TOKEN}" --ephemeral --disableupdate --replace
+/actions-runner/config.sh --unattended --labels ${LABELS} --name ${HOSTNAME} --url "https://github.com/${ORG}" --token "${TOKEN}" --ephemeral --replace
 
 /actions-runner/run.sh
 
